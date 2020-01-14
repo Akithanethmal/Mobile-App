acd41ed910ec6e14998fc89bc9a8299a0a5dfe9d
Akithanethmal <akithanethmal@gmail.com>
2020-01-14 14:32:35 +0530

    create past hires detail page

--- src/pages/PastHires.js
+++ src/pages/PastHires.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Modal,
+  TouchableHighlight,
+  Button
+} from "react-native";
 import { Icon, Card } from "react-native-elements";
 import moment from "moment";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -17,7 +25,9 @@ export default class PastHires extends Component {
     super(props);
   }
   state = {
-    data: this.data.pasthires
+    data: this.data.pasthires,
+    modalVisible: false,
+    doc: ""
   };
   componentDidMount() {}
   render() {
@@ -35,7 +45,7 @@ export default class PastHires extends Component {
             >
               <TouchableOpacity
                 style={styles.listItem}
-                onPress={() => alert("fuck u")}
+                onPress={() => this.setState({ modalVisible: true, doc: u })}
               >
                 <Icon
                   name="directions-car"
@@ -52,6 +62,106 @@ export default class PastHires extends Component {
             </Card>
           );
         })}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            this.setState({ modalVisible: false });
+          }}
+        >
+          <View style={{ marginTop: 22 }}>
+            <Card containerStyle={styles.subcardContainer}>
+              <View>
+                <View>
+                  <Text style={styles.mainText}>{this.state.doc.hireType}</Text>
+                </View>
+                <View>
+                  <Text style={styles.subText}>
+                    Date:{" "}
+                    {moment(this.state.doc.pickupDatetime).format(
+                      "MMM Do YYYY, h:mm:ss a"
+                    )}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={styles.subText}>
+                    Customer: {this.state.doc.customerName}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={styles.subText}>
+                    Vehicle: {this.state.doc.vehicleNo}
+                  </Text>
+                </View>
+              </View>
+            </Card>
+            <Card containerStyle={styles.subcardContainer}>
+              <View>
+                <Text style={styles.mainText}>Container Details</Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Container Type: {this.state.doc.containerType}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Pickup Date:{" "}
+                  {moment(this.state.doc.pickupDatetime).format(
+                    "MMM Do YYYY, h:mm:ss a"
+                  )}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Container Location: {this.state.doc.pickupLocation}
+                </Text>
+              </View>
+            </Card>
+            <Card containerStyle={styles.subcardContainer}>
+              <View>
+                <Text style={styles.mainText}>Cargo Details</Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Cargo Type: {this.state.doc.cargoType}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Cargo Weight: {this.state.doc.weight}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Vessel Arrival Date:{" "}
+                  {moment(this.state.doc.vesselArrivalDatetime).format(
+                    "MMM Do YYYY, h:mm:ss a"
+                  )}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Unloading Port: {this.state.doc.unloadingPort}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.subText}>
+                  Destination: {this.state.doc.destination}
+                </Text>
+              </View>
+            </Card>
+
+            <TouchableHighlight
+              onPress={() => {
+                this.setState({ modalVisible: false });
+              }}
+            >
+              <Text></Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
@@ -75,5 +185,31 @@ const styles = StyleSheet.create({
   itemcomponent: {
     paddingHorizontal: 10,
     fontSize: 18
+  },
+  subcardContainer: {
+    flexShrink: 1,
+    padding: 20,
+    margin: 10,
+    height: 200,
+    width: "95%",
+    justifyContent: "center",
+    borderRadius: 10
+    // backgroundColor:'#ffffff',
+    // elevation:3,
+    // cornerRadius:5,
+    // opacity:0.5,
+  },
+  mainText: {
+    textTransform: "uppercase",
+    fontSize: 35,
+    fontWeight: "bold",
+    padding: 4
+  },
+  subText: {
+    fontSize: 15,
+    padding: 4
+  },
+  detailButton: {
+    marginTop: 0
   }
 });
