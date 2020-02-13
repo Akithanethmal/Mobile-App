ec0ebfb96d044187daa1d89a9e392c368e0e3ff1
Akithanethmal <akithanethmal@gmail.com>
2020-02-13 10:55:22 +0530

    create time line

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet ,Alert} from "react-native";
+import { Icon, Card, Button } from "react-native-elements";
+import moment from "moment";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import firebase from "../../config/Firebase";
 
 export default class TimeLine extends Component {
   static navigationOptions = {
-    title: "Time Line",
+    title: "EXPORT",
     headerTitleStyle: {
       fontSize: 25,
       flex: 1
@@ -16,9 +20,86 @@ export default class TimeLine extends Component {
 
   render() {
     return (
-      <View>
-        <Text> TimeLine </Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Card containerStyle={styles.cardContainer}>
+          <View style={styles.subContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Alert.alert("Dont Press bitch")}
+            >
+              <Text style={styles.buttonText}>Truck Dispatched</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Alert.alert("Dont Press bitch")}
+            >
+              <Text style={styles.buttonText}>At Pickup Location</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Alert.alert("Dont Press bitch")}
+            >
+              <Text style={styles.buttonText}>Cargo Location</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Alert.alert("Dont Press bitch")}
+            >
+              <Text style={styles.buttonText}>In Taransit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Alert.alert("Dont Press bitch")}
+            >
+              <Text style={styles.buttonText}>Hire Completed</Text>
+            </TouchableOpacity>
+          </View>
+        </Card>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff", //#191919
+    paddingVertical: 40
+    //edting
+  },
+  DashboardTextContainer: {
+    height: "10%",
+    width: "100%",
+    alignContent: "center",
+    justifyContent: "center"
+  },
+  cardContainer: {
+    flexShrink: 1,
+    padding: 20,
+    margin: 10,
+    height: 500,
+    width: "95%",
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  button: {
+    marginTop: 7,
+    marginBottom: 7,
+    paddingVertical: 3,
+    alignItems: "center",
+    backgroundColor: "transparent",
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 5,
+    width: "100%",
+    height: 50
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "black",
+    paddingVertical: 8
+  },
+  
+});
